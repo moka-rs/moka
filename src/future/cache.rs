c0a820eee638512623e1bb4a7b3b55741faa4b16
Tatsuya Kawano <tatsuya@hibaridb.org>
2021-08-03 18:12:44 +0800

    Switch to moka-cht
    - Update future/cache module to temporary allow clippy::redundant_allocation.
      This will be addressed in Moka v0.6.0 as it requires changing the return
      type of `get_or_try_insert_with`.

--- src/future/cache.rs
+++ src/future/cache.rs
@@ -292,6 +292,10 @@ where
     /// key even if the method is concurrently called by many async tasks; only one
     /// of the calls resolves its future, and other calls wait for that future to
     /// complete.
+    #[allow(clippy::redundant_allocation)]
+    // https://rust-lang.github.io/rust-clippy/master/index.html#redundant_allocation
+    // `Arc<Box<dyn ..>>` in the return type creates an extra heap allocation.
+    // This will be addressed by Moka v0.6.0.
     pub async fn get_or_try_insert_with<F>(
         &self,
         key: K,
@@ -482,6 +486,10 @@ where
         }
     }
 
+    #[allow(clippy::redundant_allocation)]
+    // https://rust-lang.github.io/rust-clippy/master/index.html#redundant_allocation
+    // `Arc<Box<dyn ..>>` in the return type creates an extra heap allocation.
+    // This will be addressed by Moka v0.6.0.
     async fn get_or_try_insert_with_hash_and_fun<F>(
         &self,
         key: Arc<K>,
