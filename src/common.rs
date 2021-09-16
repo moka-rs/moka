107408208e3e91472e5cae2f9a6c37ae02acddbf
messense <messense@icloud.com>
2021-09-16 21:42:54 +0800

    Restore quanta on 32-bit MIPS and ARMv5TE

--- src/common.rs
+++ src/common.rs
@@ -6,9 +6,9 @@ pub(crate) mod unsafe_weak_pointer;
 
 // targe_has_atomic is more convenient but yet unstable (Rust 1.55)
 // https://github.com/rust-lang/rust/issues/32976
-// #[cfg_attr(target_has_atomic = "64", path = "common/time_quanta.rs")]
+// #[cfg_attr(target_has_atomic = "64", path = "common/time_atomic64.rs")]
 
-#[cfg_attr(feature = "atomic64", path = "common/time_quanta.rs")]
+#[cfg_attr(feature = "atomic64", path = "common/time_atomic64.rs")]
 #[cfg_attr(not(feature = "atomic64"), path = "common/time_compat.rs")]
 pub(crate) mod time;
 
