029e1686ac8189e8443af10945b2e8508b968d2e
Tatsuya Kawano <tatsuya@hibaridb.org>
2022-06-26 13:26:00 +0000

    Update entry's timestamps immediately after insertion
    Fix compile errors with `KeyDate<K>::is_dirty` method.

--- src/common/concurrent.rs
+++ src/common/concurrent.rs
@@ -77,11 +77,12 @@ impl<K> KeyDate<K> {
         &self.key
     }
 
+    // #[cfg(any(feature = "sync", feature = "future"))]
     pub(crate) fn last_modified(&self) -> Option<Instant> {
         self.entry_info.last_modified()
     }
 
-    #[cfg(any(feature = "sync", feature = "future"))]
+    // #[cfg(any(feature = "sync", feature = "future"))]
     pub(crate) fn is_dirty(&self) -> bool {
         self.entry_info.is_dirty()
     }
