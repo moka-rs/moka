691a8c1d0038665702120d3727c1d7069a9edc52
Tatsuya Kawano <tatsuya@hibaridb.org>
2022-10-22 11:38:48 +0000

    CI: Update the expected memsize of the EntryInfo struct for Rust 1.66 nightly
    - Add rustc_version crate to the build-dependencies to check the Rust version.
      (Only enabled when `RUSTFLAGS='--cfg rustver'` is set)

--- build.rs
+++ build.rs
@@ -4,5 +4,15 @@ fn main() {
     skeptic::generate_doc_tests(&["README.md"]);
 }
 
-#[cfg(not(skeptic))]
+#[cfg(rustver)]
+fn main() {
+    use rustc_version::version;
+    let version = version().expect("Can't get the rustc version");
+    println!(
+        "cargo:rustc-env=RUSTC_SEMVER={}.{}",
+        version.major, version.minor
+    );
+}
+
+#[cfg(not(any(skeptic, rustver)))]
 fn main() {}
