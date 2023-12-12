3555d405243f1d0f4e3c76756bc940df1f6f29ac
Tatsuya Kawano <tatsuya@hibaridb.org>
2023-12-12 01:03:15 +0000

    Fix the CI for nightly + minimal crate versions and also for the MSRV
    - Change the following dev dependencies for nightly + minimal crate versions:
      - Switch back to native-tls in reqwest.
    - Change the following dev dependencies for the MSRV 1.60.0:
      - Pin async-global-executor version to v2.3.1.
      - Pin blocking version to v1.4.1.

--- .ci_extras/pin-crate-vers-msrv.sh
+++ .ci_extras/pin-crate-vers-msrv.sh
@@ -5,7 +5,9 @@ set -eux
 # Pin some dependencies to specific versions for the MSRV.
 cargo update -p tempfile --precise 3.6.0
 cargo update -p tokio --precise 1.29.1
+cargo update -p async-global-executor --precise 2.3.1
 cargo update -p async-executor --precise 1.5.1
+cargo update -p blocking --precise 1.4.1
 cargo update -p reqwest --precise 0.11.18
 cargo update -p regex --precise 1.9.6
 cargo update -p memchr --precise 2.6.2
--- .ci_extras/pin-crate-vers-nightly.sh
+++ .ci_extras/pin-crate-vers-nightly.sh
@@ -3,4 +3,6 @@
 set -eux
 
 # Pin some dependencies to specific versions for the nightly toolchain.
+cargo update -p openssl --precise 0.10.39
+cargo update -p cc --precise 1.0.61
 cargo update -p proc-macro2 --precise 1.0.60
