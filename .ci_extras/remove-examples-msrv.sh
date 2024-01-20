b3fa860db7ccdc3ca4c71c92446e46ff873a0de8
Tatsuya Kawano <tatsuya@hibaridb.org>
2024-01-20 20:32:42 +0800

    CI: Disable an example `reinsert_expired_entries_sync` when testing the MSRV

--- .ci_extras/remove-examples-msrv.sh
+++ .ci_extras/remove-examples-msrv.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 
-# Remove examples from the MSRV build.
+# Disable examples from the MSRV build.
 
 set -eux
 
-# Remove this because `OnceLock` was introduced in 1.70.0.
-rm ./examples/reinsert_expired_entries_sync.rs
+# Replace the source file of example at $1 with the template file $2.
+function disable_example_sync() {
+    local example_name="$1"
+
+    mv ./examples/${example_name}.rs ./examples/${example_name}.rs.bak
+    cat << EOF > ./examples/${example_name}.rs
+fn main() {}
+EOF
+
+    echo "Disabled $example_name."
+}
+
+# `OnceLock` was introduced in 1.70.0.
+disable_example_sync reinsert_expired_entries_sync
