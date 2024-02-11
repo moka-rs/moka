fd79da5444df920f533348bed1a5fce02ef80568
Tatsuya Kawano <tatsuya@hibaridb.org>
2024-02-11 06:12:23 +0000

    Fix the CI for nightly + minimal crate versions
    - Pin `parking_lot_core` to `v0.9.3`
    - Pin `dashmap` to `v5.4.0`

--- .ci_extras/pin-crate-vers-nightly.sh
+++ .ci_extras/pin-crate-vers-nightly.sh
@@ -6,5 +6,7 @@ set -eux
 cargo update -p openssl --precise 0.10.39
 cargo update -p cc --precise 1.0.61
 cargo update -p proc-macro2 --precise 1.0.63
+cargo update -p parking_lot_core --precise 0.9.3
+cargo update -p dashmap --precise 5.4.0
 # https://github.com/tkaitchuck/aHash/issues/200
 cargo update -p ahash --precise 0.8.7
