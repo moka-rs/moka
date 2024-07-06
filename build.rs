c78027a13090be6c1402fbe6d17b5e01a505c364
Tatsuya Kawano <tatsuya@hibaridb.org>
2024-07-06 15:51:25 +0800

    Fix the CI with Rust 1.80 beta
    - Make `rustc` to allow specific custom `cfg` condition names (e.g., `rustver`).

--- build.rs
+++ build.rs
@@ -1,3 +1,15 @@
+#![allow(unexpected_cfgs)] // for `#[cfg(rustver)]` in this build.rs.
+
+const ALLOWED_CFG_NAMES: &[&str] = &[
+    "armv5te",
+    "beta_clippy",
+    "circleci",
+    "kani",
+    "mips",
+    "rustver",
+    "trybuild",
+];
+
 #[cfg(rustver)]
 fn main() {
     use rustc_version::version;
@@ -6,7 +18,18 @@ fn main() {
         "cargo:rustc-env=RUSTC_SEMVER={}.{}",
         version.major, version.minor
     );
+
+    allow_cfgs(ALLOWED_CFG_NAMES);
 }
 
 #[cfg(not(rustver))]
-fn main() {}
+fn main() {
+    allow_cfgs(ALLOWED_CFG_NAMES);
+}
+
+/// Tells `rustc` to allow `#[cfg(...)]` with the given names.
+fn allow_cfgs(names: &[&str]) {
+    for name in names.iter() {
+        println!("cargo:rustc-check-cfg=cfg({name})");
+    }
+}
