c84c8579aded2bffd524c5e766f5a8d21adeb3ff
Tatsuya Kawano <tatsuya@hibaridb.org>
2024-11-03 00:48:45 +0000

    Fix the CI for nightly + minimal crate versions
    - Unpin the proc-macro2 version.

--- .ci_extras/pin-crate-vers-nightly.sh
+++ .ci_extras/pin-crate-vers-nightly.sh
@@ -3,6 +3,5 @@
 set -eux
 
 # Pin some dependencies to specific versions for the nightly toolchain.
-cargo update -p proc-macro2 --precise 1.0.63
 # https://github.com/tkaitchuck/aHash/issues/200
 cargo update -p ahash --precise 0.8.7
