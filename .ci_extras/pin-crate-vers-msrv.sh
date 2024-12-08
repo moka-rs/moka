dc22bf5354aa59f68dfa187134987bd09280a79d
Tatsuya Kawano <tatsuya@hibaridb.org>
2024-12-08 12:50:35 +0000

    CI: Fix the CI for the MSRV 1.65
    Pin `url` to `v2.5.2` when testing the MSRV.

--- .ci_extras/pin-crate-vers-msrv.sh
+++ .ci_extras/pin-crate-vers-msrv.sh
@@ -3,8 +3,9 @@
 set -eux
 
 # Pin some dependencies to specific versions for the MSRV.
-# cargo update -p <crate> --precise <version>
-cargo update -p actix-rt --precise 2.9.0
-cargo update -p cc --precise 1.0.105
-cargo update -p tokio-util --precise 0.7.11
-cargo update -p tokio --precise 1.38.1
+# cargo update <crate> --precise <version>
+cargo update actix-rt --precise 2.9.0
+cargo update cc --precise 1.0.105
+cargo update tokio-util --precise 0.7.11
+cargo update tokio --precise 1.38.1
+cargo update url --precise 2.5.2
