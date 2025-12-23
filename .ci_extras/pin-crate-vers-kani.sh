7c4e3d10edd13751d57bc0c01bfe69e93bf269f5
Tatsuya Kawano <tatsuya@hibaridb.org>
2025-12-23 08:04:50 +0800

    fix(ci): Pin `async-lock` to `v3.4.1` to fix the Kani tests

--- .ci_extras/pin-crate-vers-kani.sh
+++ .ci_extras/pin-crate-vers-kani.sh
@@ -4,4 +4,4 @@ set -eux
 
 # Pin some dependencies to specific versions for the nightly toolchain
 # used by Kani verifier.
-# cargo update -p crate-name --precise x.y.z
+cargo update -p async-lock --precise 3.4.1
