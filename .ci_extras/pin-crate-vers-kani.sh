591546a88fca20a6c133544a47efb5f1f3cd06d5
Tatsuya Kawano <tatsuya@hibaridb.org>
2026-02-28 06:26:48 +0000

    fix(ci): Pin `uuid` to `v1.20.0` to fix the Kani tests

--- .ci_extras/pin-crate-vers-kani.sh
+++ .ci_extras/pin-crate-vers-kani.sh
@@ -5,3 +5,4 @@ set -eux
 # Pin some dependencies to specific versions for the nightly toolchain
 # used by Kani verifier.
 cargo update -p async-lock --precise 3.4.1
+cargo update -p uuid --precise 1.20.0
